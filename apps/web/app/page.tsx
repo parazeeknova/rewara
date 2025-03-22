17d2d41e0dd237386c74f31e35206114b74c4d66
Harsh Sahu <yesh8harsh+github@gmail.com>
2025-03-23 00:29:37 +0530

    feat[CONFIGS]: Added configs for TS

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,36 +1,12 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
+import Image from 'next/image';
+import styles from './page.module.css';
+// biome-ignore lint/correctness/noUnusedImports: temp fix
+import React = require('react');
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
         <ol>
           <li>
             Get started by editing <code>apps/web/app/page.tsx</code>
@@ -63,9 +39,6 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
       </main>
       <footer className={styles.footer}>
         <a
